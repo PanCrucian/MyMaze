70cb93a022aa2a5de88b6883c06904bc5379f81c
Юрий <troyura@gmail.com>
2015-03-23 19:56:22 +0300

    Добавлен и отсортирован некоторый контент

---
+++ MyMazeUnity/Assets/Scripts/MyMaze.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMaze : MonoBehaviour {
+
+    void Awake()
+    {
+        //Сохраняем инстанс в каждой сцене
+        if (Application.isPlaying)
+            DontDestroyOnLoad(gameObject);
+
+        SetupApplicationPreferences();
+    }
+
+    /// <summary>
+    /// Устанавливаем параметры приложения
+    /// </summary>
+    void SetupApplicationPreferences()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
---
+++ MyMazeUnity/Assets/Textures/UI/SpriteSheets/Readme.txt
@@ -0,0 +1,2 @@
+Used TexturePacker 3.6.0 for sprites packing
+Used plugin for Unity 5 with name TexturePackerImporter

a12f2065a56a8901c0d61fa1b0e09c4cd49c5ca4
Юрий <troyura@gmail.com>
2015-06-05 18:48:54 +0300

    Баг со стрелкой назад в выборе уровня.Баг с перемещение игрока если палец не отпускать. Баг с возвратом в главное меню если энергия кончилась, а должно в паки. Стрелочка на туториале телепорта

--- MyMazeUnity/Assets/Scripts/UI/Menu/MainMenuUI.cs
+++ MyMazeUnity/Assets/Scripts/UI/Menu/MainMenuUI.cs
@@ -4,11 +4,12 @@ using System.Collections;
 [RequireComponent(typeof(Animator))]
 public class MainMenuUI : MonoBehaviour {
 
+    public LevelsMenuUI levelsMenuUi;
+
 	void Start () {
         if (!MyMaze.Instance.IsFirstLoad)
         {
             CGSwitcher.Instance.SetHideObject(GetComponent<Animator>());
-            LevelsMenuUI levelsMenuUi = GameObject.FindObjectOfType<LevelsMenuUI>();
             CGSwitcher.Instance.SetShowObject(levelsMenuUi.GetComponent<Animator>());
             CGSwitcher.Instance.Switch();
         }

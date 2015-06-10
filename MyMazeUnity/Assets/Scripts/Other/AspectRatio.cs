6b0a494f2140b6dc36d82384ce2c1dafc60e3e3f
Юрий <troyura@gmail.com>
2015-06-10 13:07:41 +0300

    Изменил немного отступы у превью паков, для соотношейний 2:3 и 3:4. Настроен проект на релизный билд. Нет только рекламы

---
+++ MyMazeUnity/Assets/Scripts/Other/AspectRatio.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AspectRatio
+{
+    public static Vector2 GetAspectRatio(int x, int y)
+    {
+        float f = (float)x / (float)y;
+        int i = 0;
+        while (true)
+        {
+            i++;
+            if (System.Math.Round(f * i, 2) == Mathf.RoundToInt(f * i))
+                break;
+        }
+        return new Vector2((float)System.Math.Round(f * i, 2), i);
+    }
+    public static Vector2 GetAspectRatio(Vector2 xy)
+    {
+        float f = xy.x / xy.y;
+        int i = 0;
+        while (true)
+        {
+            i++;
+            if (System.Math.Round(f * i, 2) == Mathf.RoundToInt(f * i))
+                break;
+        }
+        return new Vector2((float)System.Math.Round(f * i, 2), i);
+    }
+    public static Vector2 GetAspectRatio(int x, int y, bool debug)
+    {
+        float f = (float)x / (float)y;
+        int i = 0;
+        while (true)
+        {
+            i++;
+            if (System.Math.Round(f * i, 2) == Mathf.RoundToInt(f * i))
+                break;
+        }
+        if (debug)
+            Debug.Log("Aspect ratio is " + f * i + ":" + i + " (Resolution: " + x + "x" + y + ")");
+        return new Vector2((float)System.Math.Round(f * i, 2), i);
+    }
+    public static Vector2 GetAspectRatio(Vector2 xy, bool debug)
+    {
+        float f = xy.x / xy.y;
+        int i = 0;
+        while (true)
+        {
+            i++;
+            if (System.Math.Round(f * i, 2) == Mathf.RoundToInt(f * i))
+                break;
+        }
+        if (debug)
+            Debug.Log("Aspect ratio is " + f * i + ":" + i + " (Resolution: " + xy.x + "x" + xy.y + ")");
+        return new Vector2((float)System.Math.Round(f * i, 2), i);
+    }
+}
---
+++ MyMazeUnity/Assets/Scripts/UI/PackPreview4x3Kludge.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PackPreview4x3Kludge : MonoBehaviour
+{
+
+    public float value = 13f;
+    public float value2 = 4f;
+    public RectTransform rTrans;
+    public Vector2 screenAspect;
+
+    void Start() {
+        rTrans = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        screenAspect = AspectRatio.GetAspectRatio(Camera.main.pixelWidth, Camera.main.pixelHeight);
+        if (screenAspect.x == 3 && screenAspect.y == 4)
+            rTrans.anchoredPosition = new Vector2(value, rTrans.anchoredPosition.y);
+        else if(screenAspect.x == 2 && screenAspect.y == 3)
+            rTrans.anchoredPosition = new Vector2(value2, rTrans.anchoredPosition.y);
+        else
+            rTrans.anchoredPosition = new Vector2(0, rTrans.anchoredPosition.y);
+    }
+}

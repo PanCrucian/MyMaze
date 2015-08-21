669fdb083f7d913f4eabd84d7197da99345a7f5b
Юрий <troyura@gmail.com>
2015-08-21 19:45:36 +0300

    Обработчик кнопки возврат на Android

---
+++ MyMazeUnity/Assets/Scripts/UI/AndroidBackButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidBackButton : MonoBehaviour {
+    
+#if UNITY_ANDROID || UNITY_EDITOR
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            BackButtonUI[] backBtns = GameObject.FindObjectsOfType<BackButtonUI>();
+            foreach (BackButtonUI btn in backBtns)
+                if(btn.isActiveAndEnabled)
+                    InputSimulator.Instance.SimulateClick(btn.gameObject);
+
+            return;
+        }
+    }
+#endif
+}
